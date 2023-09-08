f37af05519993194a78b5445451e135889060b0e
ranjana990 <ranjanamishra990@yahoo.com>
2023-09-08 14:42:45 +0530

    added

--- push_image.sh
+++
@@ -1,9 +0,0 @@
-
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 465781362998.dkr.ecr.us-east-1.amazonaws.com
-
-docker build . -t lambda
-docker tag lambda 465781362998.dkr.ecr.us-east-1.amazonaws.com/lambda
-docker push 465781362998.dkr.ecr.us-east-1.amazonaws.com/lambda
-
-
